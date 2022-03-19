e633ce1fe6202a8d141e81e50df1b8a25dae2feb
GhaidaRafa <101697787+GhaidaRafa@users.noreply.github.com>
2022-03-19 10:30:13 +0800

    Add files via upload

---
+++ Pra-Praktikum/kode1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char operator;
+	float a, b;
+	
+	cout<<"Program Kalkulator Sederhana"<<endl;
+	cout<<"Masukkan operator"<<endl;
+	cout<<" + (Tambah)\t - (Kurang)\n * (Kali)\t / (Bagi)";
+	cin>>operator;
+	cout<<"Masukkan angka";
+	cin>> a >> b ;	
+}
